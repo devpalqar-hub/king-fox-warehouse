9f2acada37da88c2c9c2deb4e6a1c0ba4aeca0b9
mudfayaz44 <muhdfayaz44@gmail.com>
2026-03-17 11:26:16 +0530

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Inter, Manrope } from "next/font/google";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({
